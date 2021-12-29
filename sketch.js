1f04a75a9e105da3171a7204868540ff8af0e640
vividfax <vividfax@gmail.com>
2021-12-29 09:48:04 +0000

    Add submit button and score

--- sketch.js
+++ sketch.js
@@ -5,6 +5,15 @@ let guess = "";
 
 let index = 0;
 
+let score = 0;
+let cheated = false;
+let finished = false;
+
+let shakeOffsetX = 0;
+let shakeOffsetY = 0;
+let shakeTimer = 0;
+let shake;
+
 function preload() {
 
     compoundNouns = loadStrings("./compound-nouns.txt");
@@ -20,12 +29,27 @@ function setup() {
     compoundNouns = shuffle(compoundNouns);
     getNouns();
 
-    select("#skip-button").mousePressed(() => {guess = answer; draw()});
+    select("#submit-button").mousePressed(() => {makeGuess()});
+    select("#skip-button").mousePressed(() => {guess = answer; cheated = true;draw()});
 
     noLoop();
 }
 
 function draw() {
+
+    if (shakeTimer > 12) {
+        shake = false;
+        shakeTimer = 0;
+        shakeOffsetX = 0;
+        shakeOffsetY = 0;
+        noLoop();
+    }
+
+    if (shake) {
+        shakeTimer++;
+        shakeOffsetX = random(-2, 2);
+        shakeOffsetY = random(-2, 2);
+    }
 
     push();
     translate(width/2, height/2)
@@ -33,22 +57,41 @@ function draw() {
     background("#86626E");
 
     textFont("Raleway Dots");
-    textSize(140);
+
+    if (finished) {
+        textSize(140);
+        fill("#fff");
+        text("Your score is " + score + "/" + index, 0, 0);
+        select("#submit-button").hide();
+        select("#skip-button").hide();
+        pop();
+        return;
+    }
+
+    if (index > 0) {
+        fill("#fff");
+        textSize(50);
+        text(score + "/" + index, 0, -250);
+    }
+
     noStroke();
     fill(255, 255, 255, 30);
-    rect(0, 90, 1000, 110, 5);
+    rect(0, 90, 1000 + shakeOffsetX, 110 + shakeOffsetY, 5);
+
+    textSize(140);
     fill("#fff");
     text(nouns, 0, -120);
+
     textSize(50);
     text("means", 0, -20);
     textSize(70);
 
     if (guess.length > 0) {
         fill("#fff");
-        text(guess, 0, 90);
+        text(guess, 0 + shakeOffsetX, 90 + shakeOffsetY);
     } else if (index == 0) {
         fill(255, 255, 255, 100);
-        text("", 0, 90);
+        text("Type your answer", 0 + shakeOffsetX, 90 + shakeOffsetY);
     }
 
     pop();
@@ -61,14 +104,12 @@ function keyPressed() {
         guess = guess.toLowerCase();
     }
 
-    if (keyCode == 8) {
+    if (keyCode == 8) { // keycode equals delete
         guess = guess.slice(0, -1);
     }
 
-    if (guess == answer) {
-        index++;
-        getNouns();
-        guess = "";
+    if (keyCode == 13) {
+        makeGuess();
     }
 
     draw();
@@ -76,9 +117,33 @@ function keyPressed() {
 
 function getNouns() {
 
+    if (index == compoundNouns.length) {
+        finished = true;
+        return;
+    }
+
     let phrase = compoundNouns[index];
     phrase = phrase.split("=");
 
     answer = phrase[0];
     nouns = phrase[1];
+}
+
+function makeGuess() {
+
+    if (guess == answer) {
+        if (!cheated) {
+            score++;
+        } else {
+            cheated = false;
+        }
+        index++;
+        getNouns();
+        guess = "";
+    } else {
+        shake = true;
+        loop();
+    }
+
+    draw();
 }
